21424632da108b5462d8095dadf5814e2515bf69
oalberto96 <ochoapublic@gmail.com>
2019-08-25 12:33:04 -0700

    fix overlap navbar

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import App from './App';
-import { Container } from 'semantic-ui-react';
 import MainScreenContainer from './common/features/MainScreen/MainScreenContainer';
 import ProjectsContainer from './common/features/Projects/ProjectsContainer';
 
@@ -9,13 +8,11 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Route path="/" component={App} />
-      <Container>
-        <Switch>
-          <Route path="/projects" component={ProjectsContainer} />
-          <Route path="/timer" component={MainScreenContainer} />
-          <Redirect path="/" to="/timer" />
-        </Switch>
-      </Container>
+      <Switch>
+        <Route path="/projects" component={ProjectsContainer} />
+        <Route path="/timer" component={MainScreenContainer} />
+        <Redirect path="/" to="/timer" />
+      </Switch>
     </BrowserRouter>
   );
 };
--- src/common/components/MainContainer/index.js
+++ src/common/components/MainContainer/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Container } from 'semantic-ui-react';
 import styles from './styles.module.css';
 
 const MainContainer = ({ children, title }) => {
   return (
     <div className={styles.mainContainer}>
-      <Grid>
-        <Grid.Row>
-          <Grid.Column>
-            <h1 className={styles.title}>{title}</h1>
-          </Grid.Column>
-        </Grid.Row>
-        {children}
-      </Grid>
+      <div className="main-container">
+        <Container>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column>
+                <h1 className={styles.title}>{title}</h1>
+              </Grid.Column>
+            </Grid.Row>
+            {children}
+          </Grid>
+        </Container>
+      </div>
     </div>
   );
 };
