03656c4ad1447386db19156534dec9b0c880468c
oalberto96 <ochoapublic@gmail.com>
2019-08-25 17:18:15 -0700

    Create hierarchy component

---
+++ src/common/features/Projects/HierarchyList/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { List } from 'semantic-ui-react';
+import styles from './styles.module.css';
+
+const HierarchyList = props => {
+  const [list, setList] = useState(
+    props.list.map(item => ({
+      ...item,
+      open: item.open || false,
+      children: item.children.map(child => ({
+        ...child,
+        open: child.open || false
+      }))
+    }))
+  );
+  return (
+    <List>
+      {list.map(item => (
+        <List.Item key={item.id}>
+          <List.Icon
+            name={`${item.open ? 'caret down' : 'caret right'}`}
+          ></List.Icon>
+          <List.Content>
+            <List.Header
+              className={styles.item}
+              onClick={() =>
+                setList(
+                  list.map(itemSelected => {
+                    if (itemSelected.id === item.id) {
+                      return {
+                        ...itemSelected,
+                        open: !itemSelected.open
+                      };
+                    }
+                    return itemSelected;
+                  })
+                )
+              }
+            >
+              {item.name}
+            </List.Header>
+            {item.open &&
+              item.children.map(child => (
+                <List.List key={child.id}>
+                  <List.Item>
+                    <List.Content>
+                      <List.Header className={styles.child}>
+                        {child.name}
+                      </List.Header>
+                    </List.Content>
+                  </List.Item>
+                </List.List>
+              ))}
+          </List.Content>
+        </List.Item>
+      ))}
+    </List>
+  );
+};
+
+export default HierarchyList;
--- src/common/features/Projects/ProjectsContainer/index.js
+++ src/common/features/Projects/ProjectsContainer/index.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import MainContainer from '../../../components/MainContainer';
+import { Grid } from 'semantic-ui-react';
+import HierarchyList from '../HierarchyList';
 
 export default () => {
-  return <MainContainer title="Projects">Projects</MainContainer>;
+  return (
+    <MainContainer title="Projects">
+      <Grid.Row>
+        <Grid.Column>
+          <HierarchyList
+            list={[
+              {
+                id: 6,
+                name: 'Languages',
+                open: true,
+                children: [
+                  {
+                    id: 4,
+                    name: 'English'
+                  }
+                ]
+              },
+              {
+                id: 1,
+                name: 'Tech',
+                children: [
+                  {
+                    id: 5,
+                    name: 'Ruby'
+                  }
+                ]
+              }
+            ]}
+          ></HierarchyList>
+        </Grid.Column>
+      </Grid.Row>
+    </MainContainer>
+  );
 };
