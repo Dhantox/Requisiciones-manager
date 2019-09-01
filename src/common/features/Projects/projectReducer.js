e14d0ad08c7e576718640211f97c6a3ee19a1f29
oalberto96 <ochoapublic@gmail.com>
2019-08-31 21:13:03 -0700

    fix store configuration

--- src/common/features/Projects/projectReducer.js
+++ src/common/features/Projects/projectReducer.js
@@ -1,10 +1,33 @@
-const initialState = {};
+const initialState = {
+  projects: [
+    {
+      id: 6,
+      name: 'Languages',
+      children: [
+        {
+          id: 4,
+          name: 'English'
+        }
+      ]
+    },
+    {
+      id: 1,
+      name: 'Tech',
+      children: [
+        {
+          id: 5,
+          name: 'Ruby'
+        }
+      ]
+    }
+  ],
+  selectedProject: {}
+};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case 'ADD_PROJECT':
       return { ...state, ...payload };
-
     default:
       return state;
   }
--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 import projectReducer from './common/features/Projects/projectReducer';
 
-export default function store(initialState) {
+export default function configureStore(initialState) {
   const store = createStore(
-    combineReducers(projectReducer),
+    combineReducers({ projectReducer }),
     initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
