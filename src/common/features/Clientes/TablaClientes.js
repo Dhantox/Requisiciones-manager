9c341616a71dd8d24dd5b31f847af8b8da89984e
oalberto96 <ochoapublic@gmail.com>
2019-09-07 11:09:25 -0700

    Cambiar la clase de tabla clientes por una funcion

--- src/common/features/Clientes/TablaClientes.js
+++ src/common/features/Clientes/TablaClientes.js
@@ -1,74 +1,63 @@
 import _ from 'lodash';
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Table } from 'semantic-ui-react';
 
-export default class TablaClientes extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      column: null,
-      data: props.data,
-      direction: null
-    };
-  }
+const TablaClientes = props => {
+  const [state, setState] = useState({
+    column: null,
+    data: props.data,
+    direction: null
+  });
+  const { column, data, direction } = state;
 
-  componentWillReceiveProps(props) {
-    this.setState({ ...this.state, data: props.data });
-  }
+  useEffect(() => {
+    setState({ ...state, data: props.data });
+    return () => {};
+  }, [props]);
 
-  shouldComponentUpdate = (nextProps, nextUpdate) => {
-    console.log(nextProps);
-    return this.state.data !== nextProps.data;
-  };
-
-  handleSort = clickedColumn => () => {
-    const { column, data, direction } = this.state;
-
+  const handleSort = clickedColumn => () => {
     if (column !== clickedColumn) {
-      this.setState({
+      setState({
         column: clickedColumn,
         data: _.sortBy(data, [clickedColumn]),
         direction: 'ascending'
       });
-
       return;
     }
 
-    this.setState({
+    setState({
       data: data.reverse(),
       direction: direction === 'ascending' ? 'descending' : 'ascending'
     });
   };
-
-  render() {
-    const { column, data, direction } = this.state;
-    return (
-      <Table sortable celled striped fixed>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell
-              sorted={column === 'name' ? direction : null}
-              onClick={this.handleSort('name')}
-            >
-              Nombre
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              sorted={column === 'age' ? direction : null}
-              onClick={this.handleSort('age')}
-            >
-              RFC
-            </Table.HeaderCell>
+  return (
+    <Table sortable celled striped fixed>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell
+            sorted={column === 'name' ? direction : null}
+            onClick={handleSort('name')}
+          >
+            Nombre
+          </Table.HeaderCell>
+          <Table.HeaderCell
+            sorted={column === 'age' ? direction : null}
+            onClick={handleSort('age')}
+          >
+            RFC
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {_.map(data, ({ rfc, id, nombre }) => (
+          <Table.Row key={id}>
+            <Table.Cell>{nombre}</Table.Cell>
+            <Table.Cell>{rfc}</Table.Cell>
           </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {_.map(data, ({ rfc, id, nombre }) => (
-            <Table.Row key={id}>
-              <Table.Cell>{nombre}</Table.Cell>
-              <Table.Cell>{rfc}</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
-    );
-  }
-}
+        ))}
+      </Table.Body>
+    </Table>
+  );
+};
+
+export default TablaClientes;
