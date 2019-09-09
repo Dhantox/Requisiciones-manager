4f6e77aadde2d4fed0843156d85dbc6192af9fab
oalberto96 <ochoapublic@gmail.com>
2019-09-08 18:28:42 -0700

    Expandir el componente principal

--- src/common/components/MainContainer/index.js
+++ src/common/components/MainContainer/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { Grid, Container } from "semantic-ui-react";
-import styles from "./styles.module.css";
+import React from 'react';
+import { Grid, Container } from 'semantic-ui-react';
+import styles from './styles.module.css';
 
 const MainContainer = ({ children, title, optionsButtons }) => {
   return (
     <div className={styles.mainContainer}>
       <div className="main-container">
-        <Container>
+        <Container fluid>
           <Grid>
             <Grid.Row>
               <Grid.Column width={10}>
-                <h1 className={styles.title}>{title}</h1>{" "}
+                <h1 className={styles.title}>{title}</h1>{' '}
               </Grid.Column>
               <Grid.Column floated="right" width={6}>
                 {optionsButtons}
