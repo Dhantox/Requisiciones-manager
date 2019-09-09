0da13a19364726475a4eca8cec88055162b38bd6
Daniboi <dhantox2@hotmail.com>
2019-09-09 13:33:34 -0700

    checkpoint css cotizaciones

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import { Loader } from 'semantic-ui-react';
 import './App.css';
-import NavBar from './common/components/NavBar';
+import { Link } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
+import styles from './app.module.css';
 
-function App(props) {
+export default props => {
+  let location = props.history.location.pathname;
   return (
-    <>
-      <NavBar {...props} />{' '}
-    </>
+    <body>
+    <div class="sidebar">
+      <h4></h4>
+    </div>
+    <div className={styles.navBarContainer}>
+      <div className={styles.navBar}>
+      <div className={styles.item}>
+          Menu
+          </div>
+        <div
+          className={`${
+            location === '/requisiciones' ? styles.selectedItem : styles.item
+          }`}
+        >
+          <Link to="requisiciones">
+            Requisiciones
+          </Link>
+        </div>
+        <div
+          className={`${
+            location === '/usuarios' ? styles.selectedItem : styles.item
+          }`}
+        >
+          <Link to="usuarios">
+            Usuarios
+          </Link>
+        </div>
+        <div
+          className={`${
+            location === '/clientes' ? styles.selectedItem : styles.item
+          }`}
+        >
+          <Link to="clientes">
+            Clientes
+          </Link>
+        </div>
+        <div
+          className={`${
+            location === '/requisiciones' ? styles.selectedItem : styles.item
+          }`}
+        >
+          <Link to="requisiciones">
+            <Icon name="envelope outline" size="big" />
+          </Link>
+        </div>
+      </div>
+    </div>
+    </body>
   );
-}
-
-export default App;
+};
