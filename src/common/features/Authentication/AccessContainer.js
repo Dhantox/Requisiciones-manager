66babb4e498d98534aa33e8f59441a0722481921
oalberto96 <ochoapublic@gmail.com>
2019-09-13 10:34:30 -0700

    Resolver error al aplicar funcion a los grupos en el componente access

--- src/common/features/Authentication/AccessContainer.js
+++ src/common/features/Authentication/AccessContainer.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 
 const AccessContainer = ({ access, children }) => {
-  const group = useSelector(store => store.authenticationReducer.user.group);
-  const componentToRender = null;
+  let group = useSelector(store => store.authenticationReducer.user.group);
+  group = group ? group.toLowerCase() : group;
   let isAuthorized;
   try {
     isAuthorized = access.find(
-      authorizedGroup => authorizedGroup.toLowerCase() === group.toLowerCase()
+      authorizedGroup => authorizedGroup.toLowerCase() === group
     );
   } catch (e) {
     console.error('Debes especificar el atributo access', e);
